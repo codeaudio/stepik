30618fd98a15ba88e6f5c6c5122784030168780b
codeaudio <64453492+codeaudio@users.noreply.github.com>
2020-10-15 09:31:34 +0300

    Delete login_page (1).py

--- pages/login_page (1).py
+++
@@ -1,6 +0,0 @@
-from .locators import MainPageLocators
-
-def go_to_login_page(self):
-    link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-    link.click()
-    # return LoginPage(browser=self.browser, url=self.browser.current_url) 
