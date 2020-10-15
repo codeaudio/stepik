108cc75298491a895ff4762c3c123f26c3f0ea35
codeaudio <64453492+codeaudio@users.noreply.github.com>
2020-10-15 09:27:40 +0300

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,26 +1,20 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
 
-
-
 class LoginPage(BasePage):
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
 
-    def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
+    def should_be_login_url(self)
         assert 'login' in self.browser.current_url 
-        
-
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert self.is_element_present(*LoginPageLocators.LOGIN_SUBMIT), "Login form is not presented"
+    
+        assert self.is_element_present(*LoginPageLocators.LOG_SUBMIT), "Login form is not presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert self.is_element_present(*LoginPageLocators.REGISTRATION_SUBMIT), "Registration form is not presented"
+        assert self.is_element_present(*LoginPageLocators.REG_SUBMIT), "Registration form is not presented"
 
     def register_new_user(self, email, password):
         input_email = self.browser.find_element(*LoginPageLocators.EMAIL)
@@ -32,6 +26,6 @@ class LoginPage(BasePage):
         input_confirm_password = self.browser.find_element(*LoginPageLocators.CONF_PASSWORD)
         input_confirm_password.send_keys(password)
 
-        submit = self.browser.find_element(*LoginPageLocators.REGISTRATION_SUBMIT)
+        submit = self.browser.find_element(*LoginPageLocators.REG_SUBMIT)
         submit.click()
 
